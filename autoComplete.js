96c172ee7fcfde667f59bf316a9b8966eeb34a82
John Doeua-yura <ua.vlasyuk@gmail.com>
2014-05-15 12:40:15 +0300

    Split

--- autoComplete.js
+++ autoComplete.js
@@ -13,7 +13,6 @@ function Autocomplete(vocabulary) {
     var titleEl = document.createElement("legend");
     titleEl.innerHTML = "Title";
 
-
     var field = document.createElement("input");
     field.type = "text";
     field.id = "inputtext";
@@ -28,15 +27,12 @@ function Autocomplete(vocabulary) {
 
         var sendText = field.value;   
 
-//aaaa
-
         infoSend.innerHTML = sendText;
         infoSend.className = "toserver"
     });
 
     var completer = document.createElement("div");
     completer.id = "variants";
-
 
     completer.addEventListener("click", function(e) {
 
@@ -221,66 +217,3 @@ function Autocomplete(vocabulary) {
         k = -1;
     }
 }
-
-var usefulWords = ["lift", "Lindsey", "Linda", "Linux", "list", "lirbrary", "Lihtenshtein", "Saab", "Volvo", "BMW", "apple", "orange", "donkey", "pineapple", "lemon", "peach", 'mon', 'tues', 'wed', 'thurs', 'fri', 'sat', 'sun', "John", "Susan", "Paul", "Alice"];
-
-var h1 = document.createElement("h1");
-h1.innerHTML = "My vocabulary:";
-document.body.appendChild(h1);
-
-var vocab = document.createElement("p");
-vocab.innerHTML = usefulWords.join(",    ");
-document.body.appendChild(vocab);
-
-var form = new Autocomplete(usefulWords);
-
-form.setTitle("Simle autocomplete");
-form.appendMeToBody();
-form.enableMe();
-
-var form1 = new Autocomplete(usefulWords);
-
-form1.appendMeToBody();
-form1.enableMe();
-
-var buttonSet = document.createElement("div");
-buttonSet.className = "buttonSet";
-
-var buttonTitle = document.createElement("button");
-buttonTitle.innerHTML = "Set title";
-
-var buttonSetText = document.createElement("button");
-buttonSetText.innerHTML = "Set button text";
-
-var buttonWidth = document.createElement("button");
-buttonWidth.innerHTML = "Set form width";
-
-var inputAll = document.createElement("input");
-inputAll.type = "text";
-
-buttonTitle.addEventListener("click", function(e) {
-
-    var title = inputAll.value;
-    form1.setTitle(title);
-});
-
-buttonSetText.addEventListener("click", function(e) {
-
-    var text = inputAll.value || "apply";
-    form1.setButtonText(text);
-});
-
-buttonWidth.addEventListener("click", function(e) {
-
-    var width = inputAll.value;
-    form1.setWidth(width);
-});
-
-buttonSet.appendChild(inputAll);
-buttonSet.appendChild(buttonTitle);
-buttonSet.appendChild(buttonSetText);
-buttonSet.appendChild(buttonWidth);
-document.body.appendChild(buttonSet);
-
-
-
